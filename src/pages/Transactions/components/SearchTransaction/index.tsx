7617e8bd570eae1c40ef366872518757ddab0c17
Hugo Skyrons <hugosouzaoficial4@gmail.com>
2025-06-03 20:43:36 -0300

    Search API data and fix json-server version

--- src/pages/Transactions/components/SearchTransaction/index.tsx
+++ src/pages/Transactions/components/SearchTransaction/index.tsx
@@ -3,6 +3,8 @@ import { SearchFormContainer } from "./styles";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext } from "react";
+import { TransactionsContext } from "../../../../contexts/TransactionsContext";
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -10,6 +12,7 @@ const searchFormSchema = z.object({
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 export function SearchTransaction (){
+  const { fetchTransactions } = useContext(TransactionsContext);
 
   const {
     register, 
@@ -20,9 +23,7 @@ export function SearchTransaction (){
   });
 
   async function handleSearchTransactions(data: SearchFormInputs){
-    await new Promise(resolve => setTimeout(resolve, 2000));
-
-    console.log(data);
+    await fetchTransactions(data.query);
   }
 
   return(
